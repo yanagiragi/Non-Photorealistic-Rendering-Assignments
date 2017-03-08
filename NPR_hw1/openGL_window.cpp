25d14a87e68873ff27f7057ace2e3769f3bd417a
yanagiragi <qaz90473@gmail.com>
2017-03-08 17:25:14 +0800

    trying dealing with radius
    not done yet

--- NPR_hw1/openGL_window.cpp
+++ NPR_hw1/openGL_window.cpp
@@ -21,36 +21,54 @@ void OpenGL_window :: draw()  // a.k.a. RenderFunc()
 		// draw picture
 		for(unsigned int i = 0; i  < drawcells.size(); ++i){
 				
-				glPointSize(5);	 // tmp value	
-				glColor3f(0,0,0); // black
+			glPointSize(drawcells.at(i).radius);	 // tmp value	
+			glColor3f(0,0,0); // black
 			
-				glBegin( GL_POINTS );
-					glVertex2f(
-							(drawcells[i].position.x - width/2)/width *2, 
-							(drawcells[i].position.y - height/2)/height * -2
-					);
-				glEnd();
+			glBegin( GL_POINTS );
+				glVertex2f(
+						(drawcells.at(i).position.x - width/2)/width *2, 
+						(drawcells.at(i).position.y - height/2)/height * -2
+				);
+			glEnd();
 		}
 		
 		// draw drag curve
 		for(unsigned int i = 0; i  < tmpcells.size(); ++i){
-				if(isDrag){
-					
-					glPointSize(5);	 // tmp value	
-					glColor3f(1,0,0);
-
-					glBegin( GL_POINTS );
-						glVertex2f(
-									(tmpcells[i].position.x - width/2)/width *2, 
-									(tmpcells[i].position.y - height/2)/height * -2
-							);
-					glEnd();
-				}				
+			if(isDrag){
+				glPointSize(5);	 // tmp value	
+				glColor3f(1,0,0);
+				glBegin( GL_POINTS );
+					glVertex2f(
+								(tmpcells.at(i).position.x - width/2)/width *2, 
+								(tmpcells.at(i).position.y - height/2)/height * -2
+						);
+				glEnd();
+			}				
 		}
 
 		glFlush();
 		++frame;
 }
+
+void OpenGL_window :: genRadius()
+{
+	float sum = 0;
+	
+	for (auto& n : tmpcells)
+		sum += n.count;
+	
+	float mean = sum / 2;
+	float nowcount = 0;
+	/*****************************************************************************************************/
+	for (auto& n : tmpcells){
+		nowcount += (float)n.count;
+		n.radius = abs(nowcount - mean) * RADIUSRANGE;
+		printf("%f %f\n", n.radius, (mean));
+	}	
+
+	//return 1.0;
+}
+
 void OpenGL_window :: draw_overlay()
 {
 		// Draw overlay function. 
@@ -64,13 +82,26 @@ void OpenGL_window :: draw_overlay()
 
 int OpenGL_window :: handle_mouse(int event, int button, int x, int y)
 {
+		if(lastFrame.x == x && lastFrame.y == y){
+			++ tmpcells.back().count;
+			return 1;
+		}
+		
+		// debug msg
+		//if(tmpcells.size() > 0)
+		//	printf("%f %f\n", tmpcells.back().position.x, tmpcells.back().position.y);
+
 		struct cells tempcells; // since points push back immediately, we don't use OpenGL_window:tempcell
 		struct vector2 temppos;
 		temppos.x = x;
 		temppos.y = y;
 		tempcells.position = temppos;
+		tempcells.count = 1;
 		tmpcells.push_back(tempcells);	
 			
+		lastFrame.x = x;
+		lastFrame.y = y;
+
 		return 1;
 }
 
@@ -78,8 +109,8 @@ int  OpenGL_window :: handle(int event)
 {
 		switch (event) {
 				case FL_PUSH:
-					if(callback() && (when() & FL_WHEN_CHANGED))
-                        do_callback();
+					/*if(callback() && (when() & FL_WHEN_CHANGED))
+                        do_callback();*/
                     return 1;
 				
 				case FL_DRAG:
@@ -88,10 +119,15 @@ int  OpenGL_window :: handle(int event)
 						return handle_mouse(event,Fl::event_button(), Fl::event_x(),Fl::event_y());
 
 				case FL_RELEASE:
-						printf("Release %i", tmpcells.size());
+						//printf("Release %i", tmpcells.size());
+						
+						genRadius();
+						
 						for(int i = 0; i < tmpcells.size(); ++i)
 							drawcells.push_back(tmpcells.at(i));
 						tmpcells.clear();
+						
+						lastFrame.x = lastFrame.y = -1;
 						isDrag = false;
 
 				default:
--- NPR_hw1/openGL_window.h
+++ NPR_hw1/openGL_window.h
@@ -1,7 +1,10 @@
 #ifndef  _OPENGL_WINDOW_H
 #define _OPENGL_WINDOW_H
 
+#define RADIUSRANGE 0.5
+
 #include <vector>
+#include <cmath>
 #include <FL/Fl.H>
 #include <FL/gl.h>
 #include <FL/Fl_Gl_Window.H>
@@ -16,6 +19,7 @@ struct vector2{
 struct cells{
 	float radius, ink;
 	struct vector2 position;
+	int count; // how many frames stays on it
 };
 
 class OpenGL_window : public Fl_Gl_Window { 
@@ -34,21 +38,25 @@ class OpenGL_window : public Fl_Gl_Window {
 			OpenGL_window(int x,int y,int w,int h,const char *l=0) : Fl_Gl_Window(x,y,w,h,l) 
 			{
 					mode( FL_RGB | FL_ALPHA | FL_DOUBLE | FL_STENCIL );
-					Fl::add_timeout(1.0/60.0, Timer_CB, (void*)this);
+					Fl::add_timeout(1.0/24.0, Timer_CB, (void*)this);
 					frame  = 0;
 					
 					width = w;
 					height = h;
 					
+					lastFrame.x = -1;
+					lastFrame.y = -1;
 					isDrag = false;
 
 					glViewport(x,y,w,h);
 			}
 			
+			void genRadius();
 			int handle(int event);
 			int frame, width, height;
 			bool isDrag;
-
+			
+			struct vector2 lastFrame;
 			std::vector<struct cells> tmpcells;
 			std::vector<struct cells> drawcells;
 };
